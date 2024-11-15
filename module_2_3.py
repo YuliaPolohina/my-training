ab3d687832df1ce53223d0ee51931597e82e19c0
YuliaPolohina <lulka.buka@gmail.com>
2024-11-15 11:44:59 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,4 +1,4 @@
 number_of_tasks = 12
 number_of_hours = 1.5
 Course_name = 'Python,'
-print(Course_name, 'всего задач:',number_of_tasks, 'затрачено часов:',number_of_hours, 'среднее время выполнения',(number_of_hours/number_of_tasks))
+print(Course_name, 'всего задач:',number_of_tasks,','  'затрачено часов:',number_of_hours,',' 'среднее время выполнения',(number_of_hours/number_of_tasks))
