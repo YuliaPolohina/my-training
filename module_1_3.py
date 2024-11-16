c6f2ec981e4870b2a34cfafebba81de82444bb3a
YuliaPolohina <lulka.buka@gmail.com>
2024-11-16 14:33:58 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,7 @@ name = "Юля"
 print(name)
 age = 32
 print(age)
-new_age = age + 2
-print(new_age)
-name = 'is_student'
-bool(name)
-print(bool(name))
+age = age +2
+print(age)
+is_student = True
+print(is_student)
