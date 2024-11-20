c389b64ad41f81bd124f8087ca1221659952c015
YuliaPolohina <lulka.buka@gmail.com>
2024-11-20 20:18:15 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,1 +1,9 @@
-
+my_dict = {'Mama': 123, 'Papa': 456, 'Sister': 789}
+print(my_dict)
+print(my_dict.get('Mama'))
+print(my_dict.get('Brother'))
+my_dict.update({'Grandmother': 147,
+                'Grandfather': 258})
+print(my_dict)
+my_dict.pop('Sister')
+print(my_dict)
