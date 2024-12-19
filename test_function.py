51df72cc921ae3fd4c365ed3d530a6f60289f4cc
YuliaPolohina <lulka.buka@gmail.com>
2024-12-19 09:34:28 +0300

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -4,4 +4,5 @@ def test_function():
 
     inner_function()
 
+#inner_function() - при вызове функции происходит ошибка name 'inner_function' is not defined, т.к. имя не срабатывает вне функции 
 test_function()
